63375731cd87bc8fce8c197a98645e26856dac33
likhithashwin1 <64097522+likhithashwin1@users.noreply.github.com>
2022-01-03 09:16:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import cv2
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-img = cv2.imread('1210.png')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-pytesseract
-hImg, wImg,_ = img.shape
-boxes = pytesseract.image_to_boxes(img)
-for b in boxes.splitlines():
-    print(b)
-    b = b.split(' ')
-    print(b)
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x,hImg- y), (w,hImg- h), (50, 50, 255), 1)
-    cv2.putText(img,b[0],(x,hImg- y+25),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),1)
-
-cv2.imshow('img', img)
-cv2.waitKey(0)
+import pandas as pd
+img_path = r'C:\Users\D Likhith Ashwin\PycharmProjects\pythonProject1\colorpic.jpg'
+img = cv2.imread(img_path)
+clicked = False
+r = g = b = x_pos = y_pos = 0
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+def draw_function(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b, g, r, x_pos, y_pos, clicked
+        clicked = True
+        x_pos = x
+        y_pos = y
+        b, g, r = img[y, x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+cv2.namedWindow('image')
+cv2.setMouseCallback('image', draw_function)
+while True:
+    cv2.imshow("image", img)
+    if clicked:
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
+        text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+        cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+        if r + g + b >= 400:
+            cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+        clicked = False
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+cv2.destroyAllWindows()
